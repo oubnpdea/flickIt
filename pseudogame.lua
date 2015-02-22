1acb137db82e0651d4dac12a28a8e3021e5c0bc6
sdesai191 <sdesai191@gmail.com>
2015-02-22 15:51:59 -0600

    Added Ball Image

--- pseudogame.lua
+++ pseudogame.lua
@@ -29,14 +29,16 @@ function scene:create( event )
 	-- create a grey rectangle as the backdrop
 	display.setDefault( "background", 0.1,0.2,0.3 )
   physics.setGravity(0,12)
-	ball = display.newCircle(display.contentCenterX,display.contentCenterY+200,25)
+	ball = display.newImage( "ball.png", 100, 100 )
+
+
 
   function flick(event)
       if event.phase == "began" then
         physics.setGravity(0,0)
         startX = event.x
         startY = event.y
-        physics.addBody(ball, "dynamic", {friction=1, bounce = 0.3, radius=25, isSleeping = false,filter = {maskBits = 10, categoryBits = 4}})
+        physics.addBody(ball, "dynamic", {friction=1, bounce = 0.3, radius=15, isSleeping = false})
       elseif event.phase == "moved" then
         --dragging the ball
         ball.x = event.x
