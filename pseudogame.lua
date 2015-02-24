10e5d0e43887b481f366a1214f88678644033829
JulianLaNeve <lanevejulian@gmail.com>
2015-02-23 21:55:08 -0600

    Revert c690c1e..498bc92
    This rolls back to commit c690c1e9449e862eec5f2f5fc155387886926877.

--- pseudogame.lua
+++ pseudogame.lua
@@ -34,17 +34,6 @@ function scene:create( event )
 
   line1 = display.newRect( 0, display.contentCenterY,display.contentWidth*2, 1)
   physics.addBody(line1, "static", {density = 0, friction = 0, bounce = 0, isSensor = true,filter = {maskBits = 12, categoryBits = 2}})
-<<<<<<< HEAD
-  line1:setFillColor( .07, .90, .03 )
-
-  line2 = display.newRect( 0, display.contentCenterY-60,display.contentWidth*2, 1)
-  physics.addBody(line2, "static", {density = 0, friction = 0, bounce = 0, isSensor = true,filter = {maskBits = 12, categoryBits = 2}}) --detection line, must be placed 60px above the initial line
-  line2:setFillColor( .07, .9, .03 )
-
-  line3 = display.newRect( 0, display.contentCenterY-85,display.contentWidth*2, 1)
-  physics.addBody(line3, "static", {density = 0, friction = 0, bounce = 0, isSensor = true,filter = {maskBits = 12, categoryBits = 2}})
-  line3:setFillColor(1, 0, 0)
-=======
   line1:setFillColor( 0.5 )
 
   line2 = display.newRect( 0, display.contentCenterY-60,display.contentWidth*2, 1)
@@ -53,7 +42,6 @@ function scene:create( event )
   line3 = display.newRect( 0, display.contentCenterY-85,display.contentWidth*2, 1)
   physics.addBody(line3, "static", {density = 0, friction = 0, bounce = 0, isSensor = true,filter = {maskBits = 12, categoryBits = 2}})
   line3:setFillColor( 0.5 )
->>>>>>> parent of 7edde80... reverted changes
 
   collide1 = 0
   collide2 = 0
