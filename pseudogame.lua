a269ae74dcc509b8cda3f507ae3901d2d1cf4134
JulianLaNeve <lanevejulian@gmail.com>
2015-03-11 11:11:24 -0500

    Remove unnecessary files

--- pseudogame.lua
+++ pseudogame.lua
@@ -115,7 +115,7 @@ function scene:create( event )
         ball:applyForce(a*(x1-x2), a*(y1-y2), ball.x, ball.y)
       end
       physics.setGravity(0,18)
-      display.getCurrentStage():setFocus(nil)
+      --display.getCurrentStage():setFocus(ball, nil)
   end
 
 
